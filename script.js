b156aef63bcf4df3d1537cc69858d45810d66e5b
Mitsworth SCZD <42417471+SCZD@users.noreply.github.com>
2019-07-08 19:51:36 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var containerHeight = $("#cool").height();
-var $text = $("#cool p");
- 
-while ( $text.outerHeight() > containerHeight ) {
-        $text.text(function (index, text) {
-            return text.replace(/\W*\s(\S)*$/, '...');
-       });
-}
